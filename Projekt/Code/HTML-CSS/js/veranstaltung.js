caf96fb4c1f4e1d32ebd7726d432e44e81d8b41e
Abdurrahman Karakan <abdurrahman.karakan@smail.th-koeln.de>
2017-07-15 17:11:17 +0200

    html -css -js Dateien in eingene Ordner verschoben

--- Projekt/Code/HTML-CSS/js/veranstaltung.js
+++ Projekt/Code/HTML-CSS/js/veranstaltung.js
@@ -7,15 +7,11 @@ var getTermine = dURL +'/Termine';
 function createCORSRequest(method, url) {
   var xhr = new XMLHttpRequest();
   if ("withCredentials" in xhr) {
-
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+	  
     xhr.open(method, url, true);
 
   } else if (typeof XDomainRequest != "undefined") {
 
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
     xhr = new XDomainRequest();
     xhr.open(method, url);
 
